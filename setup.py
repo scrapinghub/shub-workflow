adbd31893b9bcc6399d64dee18940e3fb3be83fb
Martin Olveyra <molveyra@gmail.com>
2019-02-12 09:29:57 -0300

    pin to boto3 version instead of botocore, in order to avoid future conflicts between boto3 installed and botocore pinned

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'jinja2>=2.7.3',
         'sqlitedict==1.6.0',
         's3fs==0.2.0',
-        'botocore==1.12.91',
+        'boto3>=1.9.92',
         'msgpack-python',
     ),
     scripts = [],
