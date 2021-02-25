0c6ff1b0edcf201e7ebf2c1319a06551609dafc0
Martin Olveyra <molveyra@gmail.com>
2021-02-25 14:09:03 -0300

    added ListCrawlManager and tests

--- tests/test_crawl_manager.py
+++ tests/test_crawl_manager.py
@@ -2,7 +2,7 @@ import os
 from unittest import TestCase
 from unittest.mock import patch
 
-from shub_workflow.crawl import CrawlManager, PeriodicCrawlManager
+from shub_workflow.crawl import CrawlManager, PeriodicCrawlManager, ListCrawlManager
 
 from .utils.contexts import script_args
 
@@ -15,6 +15,19 @@ class TestManager(CrawlManager):
 class PeriodicTestManager(PeriodicCrawlManager):
 
     name = "test"
+
+
+class ListTestManager(ListCrawlManager):
+
+    name = "test"
+    default_max_jobs = 2
+
+    def set_parameters_list(self):
+        self.parameters_list = [
+            {"argA": "valA"},
+            {"argA": "valB"},
+            {"argB": "valC"},
+        ]
 
 
 class CrawlManagerTest(TestCase):
@@ -84,3 +97,60 @@ class CrawlManagerTest(TestCase):
         mocked_super_schedule_spider.assert_any_call(
             "myspider", units=None, job_settings={}
         )
+
+    @patch("shub_workflow.crawl.WorkFlowManager.schedule_spider")
+    def test_schedule_spider_list(self, mocked_super_schedule_spider):
+        with script_args(["myspider"]):
+            manager = ListTestManager()
+
+        mocked_super_schedule_spider.side_effect = ["999/1/1", "999/1/2", "999/1/3"]
+        manager.on_start()
+
+        # first loop: schedule spider with first set of arguments
+        result = manager.workflow_loop()
+
+        self.assertTrue(result)
+        self.assertEqual(mocked_super_schedule_spider.call_count, 1)
+        mocked_super_schedule_spider.assert_any_call(
+            "myspider", units=None, argA="valA", job_settings={}
+        )
+
+        # second loop: schedule spider again with second set of arguments
+        manager.is_finished = lambda x: None
+        result = manager.workflow_loop()
+        self.assertTrue(result)
+        self.assertEqual(mocked_super_schedule_spider.call_count, 2)
+        mocked_super_schedule_spider.assert_any_call(
+            "myspider", units=None, argA="valB", job_settings={}
+        )
+
+        # third loop: still no job finished. Wait for a free slot
+        result = manager.workflow_loop()
+        self.assertTrue(result)
+        self.assertEqual(mocked_super_schedule_spider.call_count, 2)
+
+        # fourth loop: finish one job. We can schedule last one with third set of arguments
+        manager.is_finished = lambda x: "finished" if x == "999/1/1" else None
+        result = manager.workflow_loop()
+        self.assertTrue(result)
+        self.assertEqual(mocked_super_schedule_spider.call_count, 3)
+        mocked_super_schedule_spider.assert_any_call(
+            "myspider", units=None, argB="valC", job_settings={}
+        )
+
+        # fifth loop: waiting jobs to finish
+        result = manager.workflow_loop()
+        self.assertTrue(result)
+        self.assertEqual(mocked_super_schedule_spider.call_count, 3)
+
+        # fourth loop: second job finished
+        manager.is_finished = lambda x: "finished" if x == "999/1/2" else None
+        result = manager.workflow_loop()
+        self.assertTrue(result)
+        self.assertEqual(mocked_super_schedule_spider.call_count, 3)
+
+        # fifth loop: last job finished. Exit
+        manager.is_finished = lambda x: "finished" if x == "999/1/3" else None
+        result = manager.workflow_loop()
+        self.assertFalse(result)
+        self.assertEqual(mocked_super_schedule_spider.call_count, 3)
