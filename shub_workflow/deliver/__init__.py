a589abfe689c57d6b84cfde6109a82fe11b25e4e
Martin Olveyra <molveyra@gmail.com>
2021-06-16 13:24:39 -0300

    black fixes

--- shub_workflow/deliver/__init__.py
+++ shub_workflow/deliver/__init__.py
@@ -12,7 +12,8 @@ This script does the following:
 
 * searches for untagged finished jobs that corresponds to the given spiders and same flow id as inherited or passed
   to the delivery job.
-* gets all their items generated and uploads to customer s3 bucket, gs buckets or local folder, in files of given max size (either in items or bytes)
+* gets all their items generated and uploads to customer s3 bucket, gs buckets or local folder, in files of given max
+  size (either in items or bytes)
 * tags processed jobs with 'delivered' tag, so they are not processed again (tag can be customized)
 
 Deliver class is meant to be subclasses for overriding default behaviors, either via overriding of configuration class
@@ -21,13 +22,16 @@ attributes or instance methods.
 configuration class attributes
 ------------------------------
 
-DeliverScript.output_prefix - Target output prefix. Supports s3://..., gs:// and local absolute or relative folders (required)
-DeliverScript.success_file - A boolean. Whether or not to generate a finall _SUCCESS file (optional, False by default)
+DeliverScript.output_prefix - Target output prefix. Supports s3://..., gs:// and local absolute or relative folders
+                              (required)
+DeliverScript.success_file - A boolean. Whether or not to generate a finall _SUCCESS file
+                             (optional, False by default)
 
 Following environment variables are also required:
 
 In case of using s3: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, SH_APIKEY
-In case of using gs: GOOGLE_APPLICATION_CREDENTIALS (you can use helper method shub_workflow.deliver.gcstorage.set_credential_file_environ)
+In case of using gs: GOOGLE_APPLICATION_CREDENTIALS (you can use helper method
+shub_workflow.deliver.gcstorage.set_credential_file_environ)
 
 The default file format are gzipped json list. The default file name format will be:
 
@@ -60,8 +64,8 @@ _LOG = logging.getLogger(__name__)
 _LOG.setLevel(logging.INFO)
 
 
-_FIELD_RE = re.compile(r'\{field:(.+?)\}')
-_ARGUMENT_RE = re.compile(r'\{argument:(.+?)\}')
+_FIELD_RE = re.compile(r"\{field:(.+?)\}")
+_ARGUMENT_RE = re.compile(r"\{argument:(.+?)\}")
 
 
 class SqliteDictDupesFilter(object):
@@ -73,7 +77,7 @@ class SqliteDictDupesFilter(object):
         self.__filter = None
 
     def __create_db(self):
-        self.__filter = SqliteDict(self.dupes_db_file, flag='n', autocommit=True)
+        self.__filter = SqliteDict(self.dupes_db_file, flag="n", autocommit=True)
 
     def __contains__(self, element):
         if self.__filter is None:
@@ -83,7 +87,7 @@ class SqliteDictDupesFilter(object):
     def add(self, element):
         if self.__filter is None:
             self.__create_db()
-        self.__filter[element] = '-'
+        self.__filter[element] = "-"
 
     def close(self):
         if self.__filter is not None:
@@ -110,8 +114,8 @@ class OutputFile(object):
         self.__size_bytes = 0
         self.__testmode = testmode
 
-        spath = self.__key_prefix.split('/')[:-1]
-        spath.append('_SUCCESS')
+        spath = self.__key_prefix.split("/")[:-1]
+        spath.append("_SUCCESS")
         self.success_file = os.path.join(*spath)
 
     @property
@@ -134,12 +138,16 @@ class OutputFile(object):
         return gzip.open(self.filename, "wb")
 
     def write(self, item):
-        line = json.dumps(item) + '\n'
-        self.__file.write(line.encode('utf8'))
+        line = json.dumps(item) + "\n"
+        self.__file.write(line.encode("utf8"))
         self.__items_count += 1
         self.__size_bytes += sys.getsizeof(line)
-        if self.max_filesize_items > 0 and self.__items_count == self.max_filesize_items or \
-                self.max_filesize_bytes > 0 and self.__size_bytes >= self.max_filesize_bytes:
+        if (
+            self.max_filesize_items > 0
+            and self.__items_count == self.max_filesize_items
+            or self.max_filesize_bytes > 0
+            and self.__size_bytes >= self.max_filesize_bytes
+        ):
             self.flush(new_file=True)
 
     def flush(self, new_file=False):
@@ -176,8 +184,9 @@ class OutputFileDict(object):
 
     def __getitem__(self, key):
         if key not in self.__outputfiles:
-            self.__outputfiles[key] = self.outputfile_class(output_prefix=self.__output_prefix, keyprefix=key,
-                                                            testmode=self.__testmode)
+            self.__outputfiles[key] = self.outputfile_class(
+                output_prefix=self.__output_prefix, keyprefix=key, testmode=self.__testmode
+            )
         return self.__outputfiles[key]
 
     def values(self):
@@ -192,9 +201,9 @@ class OutputFileDict(object):
 
 class DeliverScript(BaseScript):
 
-    default_delivered_tag = 'delivered'
+    default_delivered_tag = "delivered"
     s3_success_file = False
-    output_prefix = ''
+    output_prefix = ""
 
     default_sh_chunk_size = 1_000
 
@@ -222,22 +231,35 @@ class DeliverScript(BaseScript):
     def add_argparser_options(self):
         super().add_argparser_options()
 
-        self.argparser.add_argument('scrapername', help='Indicate target scraper names', nargs='*')
-        self.argparser.add_argument('--output-prefix', help='Delivery prefix.', default=self.output_prefix)
-        self.argparser.add_argument('--filter-dupes-by-field', default=[],
-                                    help='Dedupe by any of the given item field. Can be given multiple times')
-        self.argparser.add_argument('--one-file-per-job', action='store_true', help='Generate one file per job.')
-        self.argparser.add_argument('--test-mode', action='store_true',
-                                    help='Run in test mode (performs all processes, but doesn\'t\
-                                          upload files nor tag jobs)')
-        self.argparser.add_argument('--sh-chunk-size', type=int, default=self.default_sh_chunk_size, help=(
-            'Chunk/page size for downloading items from Scrapy Cloud. For tweaking memory consumption and speed.'
-            ' Note that the performance will depend on the sizes of individual items in the cloud.'
-        ))
-        self.argparser.add_argument('--delivered-tag', help='Tag to apply to delivered jobs.', default=self.default_delivered_tag)
+        self.argparser.add_argument("scrapername", help="Indicate target scraper names", nargs="*")
+        self.argparser.add_argument("--output-prefix", help="Delivery prefix.", default=self.output_prefix)
+        self.argparser.add_argument(
+            "--filter-dupes-by-field",
+            default=[],
+            help="Dedupe by any of the given item field. Can be given multiple times",
+        )
+        self.argparser.add_argument("--one-file-per-job", action="store_true", help="Generate one file per job.")
+        self.argparser.add_argument(
+            "--test-mode",
+            action="store_true",
+            help="Run in test mode (performs all processes, but doesn't\
+                                          upload files nor tag jobs)",
+        )
+        self.argparser.add_argument(
+            "--sh-chunk-size",
+            type=int,
+            default=self.default_sh_chunk_size,
+            help=(
+                "Chunk/page size for downloading items from Scrapy Cloud. For tweaking memory consumption and speed."
+                " Note that the performance will depend on the sizes of individual items in the cloud."
+            ),
+        )
+        self.argparser.add_argument(
+            "--delivered-tag", help="Tag to apply to delivered jobs.", default=self.default_delivered_tag
+        )
 
     def gen_keyprefix(self, scrapername, job, item):
-        formatted_datetime = self.start_datetime.strftime('%Y-%m-%dT%H:%M:%S')
+        formatted_datetime = self.start_datetime.strftime("%Y-%m-%dT%H:%M:%S")
         return os.path.join(scrapername, formatted_datetime)
 
     def _process_job_items(self, scrapername, spider_job):
@@ -312,12 +334,13 @@ class DeliverScript(BaseScript):
 
         while True:
             jobs_count = 0
-            for spider_job in self.get_project().jobs.iter(spider=scrapername, state="finished", lacks_tag=self.args.delivered_tag,
-                                                           has_tag=has_tag, start=start):
+            for spider_job in self.get_project().jobs.iter(
+                spider=scrapername, state="finished", lacks_tag=self.args.delivered_tag, has_tag=has_tag, start=start
+            ):
                 jobs_count += 1
-                sj = self.get_project().jobs.get(spider_job['key'])
+                sj = self.get_project().jobs.get(spider_job["key"])
                 self._process_job_items(scrapername, sj)
-                jobs_to_tag.append(spider_job['key'])
+                jobs_to_tag.append(spider_job["key"])
             if jobs_count == 0:
                 break
             start += jobs_count
@@ -334,8 +357,8 @@ class S3DeliverScript(DeliverScript):
         self.output_prefix = self.s3_bucket_name
         super().__init__()
 
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format=("%(asctime)s [%(levelname)s] %(pathname)s:%(lineno)d %(message)s"))
     deliver = DeliverScript()
     deliver.run()
