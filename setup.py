220b4399183172dd5af885637824dd85bf93e2fa
Martin Olveyra <molveyra@gmail.com>
2021-09-06 12:31:17 -0300

    updated to version 1.7.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'shub-workflow',
-    version      = '1.7.8',
+    version      = '1.7.9',
     description  = 'Workflow manager for scrapinghub ScrapyCloud tasks.',
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
