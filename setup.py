aab847881060313ed369c775e1ca38df1e27fdc5
Martin Olveyra <molveyra@gmail.com>
2022-10-26 19:29:21 -0300

    update to version 1.10.7.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shub-workflow",
-    version="1.10.7.1",
+    version="1.10.7.2",
     description="Workflow manager for scrapinghub ScrapyCloud tasks.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
