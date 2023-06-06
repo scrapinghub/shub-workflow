a7bf2bf3436c43ff6903f1d07b87061b2c63c0e3
Martin Olveyra <molveyra@gmail.com>
2023-06-06 10:35:10 -0300

    make installation of gcs and s3 depedencies optional

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shub-workflow",
-    version="1.11.9.1",
+    version="1.12",
     description="Workflow manager for Zyte ScrapyCloud tasks.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -17,15 +17,17 @@ setup(
         "scrapinghub[msgpack]>=2.3.1",
         "jinja2>=2.7.3",
         "sqlitedict==1.6.0",
-        "s3fs>=0.4.0",
         "boto3>=1.9.92",
-        "google-cloud-storage>=1.38.0",
         "bloom-filter2==2.0.0",
         "collection-scanner",
         "tenacity",
         "typing-extensions",
         "scrapy",
     ),
+    extras = {
+        "with-s3-tools": "s3fs>=0.4.0",
+        "with-gcs-tools": "google-cloud-storage>=1.38.0",
+    },
     scripts=[],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
