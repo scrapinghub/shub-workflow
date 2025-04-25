a0c84158ffed74e973e74823ee8d7949a7431a32
Martin Olveyra <molveyra@gmail.com>
2025-04-25 17:13:23 -0300

    update version to 1.14.20.16

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shub-workflow",
-    version="1.14.20.15",
+    version="1.14.20.16",
     description="Workflow manager for Zyte ScrapyCloud tasks.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
